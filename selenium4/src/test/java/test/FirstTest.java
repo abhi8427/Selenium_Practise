6bec73a687cf2b5684de0c6a88844616c71e155d
Abhishek <abhishek.coolabhi@gmail.com>
2024-09-28 21:37:37 +0530

    Selenium script to add one user

--- selenium4/src/test/java/test/FirstTest.java
+++ selenium4/src/test/java/test/FirstTest.java
@@ -12,20 +12,24 @@ public class FirstTest {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("file:///D:/Study/Front%20End%20Development/JavaScript/Practise/CRUD001/index.html");
-		Thread.sleep(1000);
-		driver.findElement(By.id("data")).click();
-		driver.findElement(By.id("data")).sendKeys("Abhishek Kumar");
-		driver.findElement(By.id("adduser")).click();driver.findElement(By.id("data")).click();
-		driver.findElement(By.id("data")).clear();
-		Thread.sleep(1000);
-		driver.findElement(By.id("data")).sendKeys("Akshita Trived");
-		driver.findElement(By.id("adduser")).click();
-		driver.findElement(By.id("data")).clear();
-		Thread.sleep(1000);
-		driver.findElement(By.id("data")).click();
-		driver.findElement(By.id("data")).sendKeys("Shashi Sharma");
-		driver.findElement(By.id("adduser")).click();
-		driver.findElement(By.id("data")).clear();
+		
+		  Thread.sleep(1000); driver.findElement(By.id("data")).click();
+		  driver.findElement(By.id("data")).sendKeys("Abhishek Kumar");
+		  driver.findElement(By.id("adduser")).click();driver.findElement(By.id("data")
+		  ).click(); driver.findElement(By.id("data")).clear();
+		 
+		/*
+		 * Thread.sleep(1000);
+		 * driver.findElement(By.id("data")).sendKeys("Akshita Trived");
+		 * driver.findElement(By.id("adduser")).click();
+		 * driver.findElement(By.id("data")).clear();
+		 */
+		/*
+		 * Thread.sleep(1000); driver.findElement(By.id("data")).click();
+		 * driver.findElement(By.id("data")).sendKeys("Shashi Sharma");
+		 * driver.findElement(By.id("adduser")).click();
+		 * driver.findElement(By.id("data")).clear();
+		 */
 		
 		//driver.close();
 		
